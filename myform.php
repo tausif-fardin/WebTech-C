5e4155228cbff91be206046a88a581c14ee90b96
tausif <tausiffardin4@gmail.com>
2021-02-09 14:40:25 +0600

    space changes

--- myform.php
+++ myform.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <form action="control/results.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit" value= "SUBMIT">
 </form>
-
 </body>
 </html>
