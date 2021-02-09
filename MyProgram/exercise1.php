5364bf45ecfd91c258f75c3a8139bcd3e386668d
tausif-fardin <58599676+tausif-fardin@users.noreply.github.com>
2021-02-09 15:09:38 +0600

    create exercise1.php

---
+++ MyProgram/exercise1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <title>Image & Link</title>
+</head>
+<body>
+<img src="img/google-new-logo-png-5.png" alt="Google" width="500" height="300">
+<a href="/MyProgram/homepage.php">Have a look at my first exercise.</a>
+</body>
+</html>
