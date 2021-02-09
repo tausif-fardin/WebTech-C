9469407b96e0a4aef674397f5997d34ecd1271f4
tausif-fardin <58599676+tausif-fardin@users.noreply.github.com>
2021-02-09 15:06:13 +0600

    Create registration.php

---
+++ LabTask1/registration.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Registration Page </title>
+</head>
+<body>
+    <h1>My Registration Page</h1>
+    <form>
+    <table>
+        <tr> 
+            <td>Name:</td>
+            <td><input type="text" name="firstName"> </td>
+        </tr>     
+        <tr>
+            <td>Email:</td>
+            <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td>User Name:</td>
+            <td><input type="text" name="userName"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="passsword"></td>
+        </tr>
+        <tr>
+            <td>Confirm Password:</td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td>Gender</td>
+            <td><input type="radio" id="male" name="gender" value="male">
+            <label for="male">Male</label>
+            <input type="radio" id="female" name="gender" value="female">
+            <label for="female">Female</label>
+            <input type="radio" id="other" name="gender" value="other">
+            <label for="other">Other</label></td>
+        </tr>
+        <tr>
+            <td>Date of Birth <br></td>
+            <td><input type="date" name="birthday"></td>
+        </tr>
+        <tr>
+            
+            <td><input type="submit" value="Submit">
+            <input type="reset"></td>
+            
+
+        </tr>
+
+    </table>
+
+    </form>
+</body>
+
+</html>
