0bb83726e0ccab430cc92abecafa867028ac1eb9
tausif-fardin <58599676+tausif-fardin@users.noreply.github.com>
2021-02-09 15:07:29 +0600

    Create MyCircle.php

---
+++ MyProgram/MyCircle.php
@@ -0,0 +1,31 @@
+<?php
+
+class MyCircle{
+    private $radius;
+
+    public function __construct($radius=1){
+        $this->radius = $radius;
+    }
+    public function getRadius()
+    {
+        return $this->$radius;
+    }
+    public function setRadius($radius)
+    {
+        $this->radius = $radius;
+    }
+    public function getArea()
+    {
+        return $this->radius*$this->radius*3.1416;
+    }
+
+    public function __toString(){
+        return $this->getArea();
+    }
+}
+
+$c1 =new MyCircle();
+$c1->setRadius(8);
+echo "Area is ".$c1->__toString();
+
+?>
