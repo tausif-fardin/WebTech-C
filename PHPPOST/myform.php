fdfc7fd930cbba71adc8955da1163132ff5897d7
tausif <tausiffardin4@gmail.com>
2021-02-14 14:28:33 +0600

    Update myform.php

--- PHPPOST/myform.php
+++ PHPPOST/myform.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form action="control/results.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+<?php>
+
+$validatename="";
+$validatemail="";
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+    if(empty($name))
+{
+    $validatename = "You must enter your name.";
+}
+else{
+    $validatename = "Your name is ".$name;
+}
+
+if(empty($email))
+{
+    $validatemail = "You must enter email.";
+}
+else{
+    $validatemail = "Your email is ".$email;
+}
+}
+?>
+<php?>
+<form action="<?php echo $_SERVER["PHP_SELF"];?> method="post">
+Name: <input type="text" name="name"><?php echo $validatename;?><br>
+E-mail: <input type="text" name="email"><?php echo $validatename;?><br>
 <input type="submit" value= "SUBMIT">
 </form>
 </body>
