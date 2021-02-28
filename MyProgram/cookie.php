916491a350b65d6597ddc401c3a8ba1c920361d4
tausif <tausiffardin4@gmail.com>
2021-02-28 15:18:33 +0600

    Added cookie and exercise

--- MyProgram/cookie.php
+++ MyProgram/cookie.php
@@ -3,7 +3,6 @@ $cookie_name = "user";
 $cookie_value = "Alex Porter";
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 ?>
-<!DOCTYPE html>
 <html>
 <body>
 <?php
@@ -14,11 +13,5 @@ echo "Cookie '" . $cookie_name . "' is set!<br>";
 echo "Value is: " . $_COOKIE[$cookie_name];
 }
 ?>
-<head>
-    <title>Image & Link</title>
-</head>
-    <img src="img/google-new-logo-png-5.png" alt="Google" width="500" height="300">
-    <a href="/MyProgram/homepage.php">Have a look at my first exercise.</a>
 </body>
-
 </html>
