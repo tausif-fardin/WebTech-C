f2e0155bb5d90ce82294ba207a258c23e8d5d356
tausif <tausiffardin4@gmail.com>
2021-03-02 14:29:17 +0600

    added db

--- MyProgram/db.php
+++ MyProgram/db.php
@@ -2,14 +2,24 @@
 $servername = "localhost"; //default
 $username = "root";
 $password = ""; //default
+$dbname = "mydb";
 
 //create connection using mysql function
-$conn = new mysqli($servername,$username,$password);
+$conn = new mysqli($servername,$username,$password,$dbname);
 //check connection
 if($conn->connect_error)
 {
     die("Connection failed: ".$conn->connect_error);
 }
 echo "Connected successfully";
+$sql = "INSERT INTO user(firstname, lastname, age, id)
+VALUES ('Doe', 'Bob', 18,'18-38853-3')";
 
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
